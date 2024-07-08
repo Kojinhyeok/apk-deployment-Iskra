f9ed09a3413c0fc677b21ffb3fa43b5525ea9a2d
Kojinhyeok <jinhyeokko12@gmail.com>
2024-07-08 20:53:04 +0900

    Update build.yml and run.spec to include chromedriver

--- run.py
+++ run.py
@@ -5,14 +5,24 @@ import platform
 import os
 
 def open_browser():
+    print("Opening browser...")
     webbrowser.open_new("http://127.0.0.1:5000/")
 
 if __name__ == "__main__":
     system_arch = platform.machine()
     if system_arch == 'arm64':
-        os.environ['PATH'] += os.pathsep + os.path.abspath('chromedriver_mac_arm')
+        chromedriver_path = os.path.abspath('chromedriver_mac_arm')
     else:
-        os.environ['PATH'] += os.pathsep + os.path.abspath('chromedriver_mac_x')
-        
+        chromedriver_path = os.path.abspath('chromedriver_mac_x')
+
+    if os.path.exists(chromedriver_path):
+        os.environ['PATH'] += os.pathsep + chromedriver_path
+        print(f"Added chromedriver to PATH: {chromedriver_path}")
+    else:
+        print(f"Chromedriver not found at path: {chromedriver_path}")
+    
     Timer(1, open_browser).start()
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        print(f"Error starting Flask server: {e}")
